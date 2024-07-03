219f941c536214870f8a1db27bb347c94f581934
jacob123-spade <jhm1834@gmail.com>
2024-07-03 16:41:07 +0900

    웹사이트 완성

---
+++ python.py
@@ -0,0 +1,6 @@
+html = open("html.txt","w",encoding = "utf8")
+html.write("<h1></h1> tag can be used when you should need heads")
+html.write("""<p></p> tag and <br> tag are used when you need to space. Especially <p></p> tag can use when you distribute paragraphs.""")
+
+html.write("""<ol></ol> and <ul></ul> can be used when you express the list and distribute it. And also <li></li> tag can be used when you need list.""")
+html.write("""<strong></strong> can use when you need to highlight the words. And <u></u> tag can be used when you should underline the words. """)
